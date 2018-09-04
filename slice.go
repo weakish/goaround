78ee5ae3e0d14ab3def91127107484b7b9a513b2
Jakukyo Friel <weakish@gmail.com>
2018-09-05 00:53:15 +0800

    slice: add support for negative index.
    If index is negative, then returns item at (length + index).
    If (length + index) is still negative, returns item at 0.

--- slice.go
+++ slice.go
@@ -3,39 +3,68 @@ package goaround
 // GetByte indexes string with bound safety.
 func GetByte(someString string, index int) byte {
 	var length int = len(someString)
-	if index < length {
-		return someString[index]
-	} else {
+	if index < 0 {
+		var negatived int = length + index
+		if negatived < 0 {
+			return someString[0]
+		} else {
+			return someString[negatived]
+		}
+	} else if index >= length {
 		return someString[length - 1]
+	} else {
+		return someString[index]
 	}
 }
 
 // GetBool indexes slices of booleans with bound safety.
 func GetBool(booleanSlice []bool, index int) bool {
 	var length int = len(booleanSlice)
-	if index < length {
-		return booleanSlice[index]
-	} else {
+	if index < 0 {
+		var negatived int = length + index
+		if negatived < 0 {
+			return booleanSlice[0]
+		} else {
+			return booleanSlice[negatived]
+		}
+
+	} else if index >= length {
 		return booleanSlice[length - 1]
+	} else {
+		return booleanSlice[index]
 	}
 }
 
 // GetString indexes slices of strings with bound safety.
 func GetString(stringSlice []string, index int) string {
 	var length int = len(stringSlice)
-	if index < length {
-		return stringSlice[index]
-	} else {
+	if index < 0 {
+		var negatived int = length + index
+		if negatived < 0 {
+			return stringSlice[0]
+		} else {
+			return stringSlice[negatived]
+		}
+	} else if index >= length {
 		return stringSlice[length - 1]
+	} else {
+		return stringSlice[index]
 	}
 }
 
 // GetInt indexes slices of integers with bound safety.
 func GetInt(intSlice []int, index int) int {
 	var length int = len(intSlice)
-	if index < length {
-		return intSlice[index]
-	} else {
+	if index < 0 {
+		var negatived int = length + index
+		if negatived < 0 {
+			return intSlice[0]
+		} else {
+			return intSlice[negatived]
+		}
+	} else if index >= length {
 		return intSlice[length - 1]
+	} else {
+		return intSlice[index]
 	}
 }
--- slice_test.go
+++ slice_test.go
@@ -13,6 +13,14 @@ func ExampleGetByteExceeding() {
 	fmt.Println(GetByte("goaround", 100))
 	// Output: 100
 }
+func ExampleGetByteNegative() {
+	fmt.Println(GetByte("goaround", -1))
+	// Output: 100
+}
+func ExampleGetByteLeftExceeding() {
+	fmt.Println(GetByte("goaround", -100))
+	// Output: 103
+}
 
 func ExampleGetBool() {
 	fmt.Println(GetBool([]bool{true, false}, 0))
@@ -21,6 +29,14 @@ func ExampleGetBool() {
 func ExampleGetBoolExceeding() {
 	fmt.Println(GetBool([]bool{true, false}, 100))
 	// Output: false
+}
+func ExampleGetBoolNegative() {
+	fmt.Println(GetBool([]bool{true, false}, -1))
+	// Output: false
+}
+func ExampleGetBoolLeftExceeding() {
+	fmt.Println(GetBool([]bool{true, false}, -100))
+	// Output: true
 }
 
 func ExampleGetInt() {
@@ -31,6 +47,14 @@ func ExampleGetIntExceeding() {
 	fmt.Println(GetInt([]int{1, 2, 3}, 100))
 	// Output: 3
 }
+func ExampleGetIntNegative() {
+	fmt.Println(GetInt([]int{1, 2, 3}, -1))
+	// Output: 3
+}
+func ExampleGetIntLeftExceeding() {
+	fmt.Println(GetInt([]int{1, 2, 3}, -100))
+	// Output: 1
+}
 
 func ExampleGetString() {
 	fmt.Println(GetString([]string{"go", "around"}, 0))
@@ -39,4 +63,12 @@ func ExampleGetString() {
 func ExampleGetStringExceeding() {
 	fmt.Println(GetString([]string{"go", "around"}, 100))
 	// Output: around
+}
+func ExampleGetStringNegative() {
+	fmt.Println(GetString([]string{"go", "around"}, -1))
+	// Output: around
+}
+func ExampleGetStringLeftExceeding() {
+	fmt.Println(GetString([]string{"go", "around"}, -100))
+	// Output: go
 }
