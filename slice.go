550c2d2b9f642d9a185cdd83da8e7620aaa0c15c
Jakukyo Friel <weakish@gmail.com>
2018-09-05 01:58:03 +0800

    refactor slice: extract index manipulation to normalizeIndex

--- slice.go
+++ slice.go
@@ -1,69 +1,48 @@
 package goaround
 
-// GetByte indexes string with bound safety.
-func GetByte(someString string, index int) byte {
-	var length int = len(someString)
+func normalizeIndex(index int, length int) int {
 	if index < 0 {
 		var negatived int = length + index
 		if negatived < 0 {
-			return someString[0]
+			return 0
 		} else {
-			return someString[negatived]
+			return negatived
 		}
 	} else if index >= length {
-		return someString[length - 1]
-	} else {
-		return someString[index]
+		return length - 1
+	} else{
+		return index
 	}
+}
+
+// GetByte indexes string with bound safety.
+func GetByte(s string, index int) byte {
+	var length int = len(s)
+	var normalizedIndex int = normalizeIndex(index, length)
+	return s[normalizedIndex]
 }
 
 // GetBool indexes slices of booleans with bound safety.
-func GetBool(booleanSlice []bool, index int) bool {
-	var length int = len(booleanSlice)
-	if index < 0 {
-		var negatived int = length + index
-		if negatived < 0 {
-			return booleanSlice[0]
-		} else {
-			return booleanSlice[negatived]
-		}
-
-	} else if index >= length {
-		return booleanSlice[length - 1]
-	} else {
-		return booleanSlice[index]
-	}
+func GetBool(s []bool, index int) bool {
+	var length int = len(s)
+	var normalizedIndex int = normalizeIndex(index, length)
+	return s[normalizedIndex]
 }
 
 // GetString indexes slices of strings with bound safety.
-func GetString(stringSlice []string, index int) string {
-	var length int = len(stringSlice)
-	if index < 0 {
-		var negatived int = length + index
-		if negatived < 0 {
-			return stringSlice[0]
-		} else {
-			return stringSlice[negatived]
-		}
-	} else if index >= length {
-		return stringSlice[length - 1]
-	} else {
-		return stringSlice[index]
-	}
+func GetString(s []string, index int) string {
+	var length int = len(s)
+	var normalizedIndex int = normalizeIndex(index, length)
+	return s[normalizedIndex]
 }
 
 // GetInt indexes slices of integers with bound safety.
-func GetInt(intSlice []int, index int) int {
-	var length int = len(intSlice)
-	if index < 0 {
-		var negatived int = length + index
-		if negatived < 0 {
-			return intSlice[0]
-		} else {
-			return intSlice[negatived]
-		}
-	} else if index >= length {
-		return intSlice[length - 1]
+func GetInt(s []int, index int) int {
+	var length int = len(s)
+	var normalizedIndex int = normalizeIndex(index, length)
+	return s[normalizedIndex]
+}
+
 	} else {
 		return intSlice[index]
 	}
