43d06b55e476f5d6e3b12be55bfb504c05305a48
Jang Rush <weakish@gmail.com>
2020-12-13 22:45:13 +0800

    :fire: Expect
    This is sugar, not workaround.
    I will move it into gosugar.

--- error.go
+++
@@ -1,9 +0,0 @@
-package goaround
-
-// Inspired by Rust.
-func Expect(err error, message string) {
-	if err != nil {
-		println(message)
-		panic(err)
-	}
-}
--- error_test.go
+++
@@ -1,6 +0,0 @@
-package goaround
-
-func ExampleExpect() {
-	var err error
-	Expect(err, "error message")
-}
